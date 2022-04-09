20e8da995c05c0305408126ba74b21d9440c2ccc
Saitss <saitsutss@gmail.com>
2022-04-09 21:32:32 +0300

    I forgot to commit these files. These files should have been in the commit dc12c4a

---
+++ src/main/java/com/Player/Player.java
@@ -0,0 +1,37 @@
+package com.Player;
+
+public class Player {
+
+    private int position;
+    private int money;
+    private PlayerState state;
+
+    Player(){
+        position = 0;
+        money = 1500;
+        state = new PlayerNotPrison();
+    }
+
+    protected void MoneyTransition(int amount){
+        money = money + amount;
+    }
+
+    protected void MovePlayer(int move){
+            if(position + move >= 20 ) MoneyTransition(1500);
+            position = (position + move) / 20;
+    }
+
+    public boolean Turn(){
+
+        boolean doubleDice;
+
+        doubleDice = state.Play(this);
+
+        if(doubleDice) Turn();
+        else state = state.EndPlay();
+
+        return money >= 0 ;
+    }
+
+
+}
